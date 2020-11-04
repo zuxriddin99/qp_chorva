984434b482bcd162c34b74e23bc871b057458df9
zuxriddin99 <zux.828@gmail.com>
2020-11-04 15:45:55 +0500

    s

--- appmain/admin.py
+++ appmain/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+class AboutCompanyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'show')
+    search_fields = ('title', 'description')
+    list_filter = ('title', 'show')
+
+
+admin.site.register(AboutCompany, AboutCompanyAdmin)
