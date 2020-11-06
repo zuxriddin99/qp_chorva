2bac56d34b0edfc4b75f42c8e84968d861ec2951
Zuxriddin <56336321+zuxriddin99@users.noreply.github.com>
2020-11-06 14:46:09 +0500

    1

--- appmain/models.py
+++ appmain/models.py
@@ -23,8 +23,8 @@ class AboutPage(models.Model):
 
 class OurAdvantage(models.Model):
     text = models.CharField(max_length=100)
+    image_2 = models.ImageField(upload_to='about_page', verbose_name='main image')
     image = models.ImageField(upload_to='about_page', blank=True, verbose_name='image')
-    image_2 = models.ImageField(upload_to='about_page', blank=True, verbose_name='images')
 
     class Meta:
         verbose_name = 'пост: наше преимущество'
